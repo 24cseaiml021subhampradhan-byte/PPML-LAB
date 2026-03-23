2acd25a67522776b243f53b7cd9117493c38703f
Subham pradhan <24cseaiml021.subhampradhan@giet.edu>
2026-03-23 08:14:51 +0530

    Add files via upload

---
+++ exp12/no.4.py
@@ -0,0 +1,27 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+sizes=[15,30,45,10]
+labels=['A','B','C','D']
+colors=['gold','yellowgreen','lightcoral','lightskyblue']
+explode=(0,0.1,0,0)
+
+plt.pie(sizes, explode=explode, labels=labels, colors=colors,
+        autopct='%1.1f%%', shadow=True, startangle=140)
+
+plt.title("Pie Chart")
+plt.axis('equal')
+plt.show()
+
+
+x=np.linspace(-5,5,50)
+y=np.linspace(-5,5,50)
+
+x,y=np.meshgrid(x,y)
+
+z=np.sin(np.sqrt(x**2 + y**2))
+
+plt.contour(x,y,z,levels=10,cmap='viridis')
+plt.title("Contour Plot")
+plt.colorbar()
+plt.show()
